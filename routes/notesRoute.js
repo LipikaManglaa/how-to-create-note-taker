58a53260a3efc18657e3bf0a08baf819c61077e4
lipika.manglaa <lipika.mangla.web@gmail.com>
2023-08-08 15:11:15 +1000

    dfdf

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -13,7 +13,7 @@ notes.get('/api/notes', (req, res) => {
   // POST /api/notes should receive a new note to save on the request body, 
   // add it to the db.json file, and then return the new note to the client. 
   notes.post('/api/notes', (req, res) => {
-   db=fs.readFileSync('./db/db.json').then((data) => res.json(JSON.parse(data)))
+  fs.readFileSync('./db/db.json').then((data) => res.json(JSON.parse(data)))
   
     // creating body for note
     const { title,text } = req.body;
@@ -28,56 +28,28 @@ notes.get('/api/notes', (req, res) => {
           db.push(newNotes);
           fs.writeFileSync('db/db.json', JSON.stringify(db));
           res.json(db);
-// readAndAppend(newNotes, 'db/db.json');
+readAndAppend(newNotes, 'db/db.json');
 
-//     const response = {
-//       status: 'success',
-//       body: newNotes,
-//     };
+    const response = {
+      status: 'success',
+      body: newNotes,
+    };
 
-//     res.json(response);
-//   }
-//    else {
-//     res.json('Error in posting notes');
-//   }
+    res.json(response);
+  }
+   else {
+    res.json('Error in posting notes');
+  }
 
         
-        }
-  });
+        })
 
 
 
 
 
 
-// notes.get('/', (req, res) =>
-// fs.readFileSync('./db/db.json').then((data) => res.json(JSON.parse(data)))
-// );
 
-// POST Route for submitting notes
-// notes.post('/', (req, res) => {
-//     // Destructuring assignment for the items in req.body
-//     const { title,text } = req.body;
 
-//   // If all the required properties are present
-//   if (title && text) {
-//     const newFeedback = {
-//        title,
-//        text,
-//      notes_id: uuid(),
-//       };
-
-//       readAndAppend(newNotes, './db/db.json');
-
-//     const response = {
-//       status: 'success',
-//       body: newNotes,
-//     };
-
-//     res.json(response);
-//   } else {
-//     res.json('Error in posting notes');
-//   }
-// });
 
 module.exports = notes;
